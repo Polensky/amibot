9fd4030130df23a363f63b0782884c8f01411fe0
Charles <sirois.charles@protonmail.com>
2020-03-10 13:14:15 -0400

    corona

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import os
+import json
+import requests
 import sigle_logger
 import datetime
 import re
@@ -187,6 +189,24 @@ async def todo(ctx):
                     color=0x006534
                     )
                 await ctx.send(embed=embed)
+
+@bot.command(name='corona', help='COVID-19')
+async def corona(ctx):
+    res = requests.get('https://coronavirus-tracker-api.herokuapp.com/all')
+    j = json.loads(res.content)
+    confirmed = j['latest']['confirmed']
+    deaths = j['latest']['deaths']
+    recovered = j['latest']['recovered']
+    embed=discord.Embed(
+        title=f':beer: Corona update :skull:',
+        url='https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6',
+        color=0x006534
+        )
+    embed.add_field(name="Confirmés", value=confirmed)
+    embed.add_field(name="Morts", value=deaths)
+    embed.add_field(name="Rétablis", value=recovered)
+    await ctx.send(embed=embed)
+
 
 bot.run(token)
 
