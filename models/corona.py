0e8968acc6f933c973f4c1ac1d50425145c2507b
Charles <sirois.charles.@protonmail.com>
2020-03-14 19:29:44 -0400

    format corona fix

--- models/corona.py
+++ models/corona.py
@@ -8,16 +8,16 @@ def my_embed() -> discord.Embed:
     """Generate embed for corona command"""
     res = requests.get('https://coronavirus-tracker-api.herokuapp.com/all')
     j = json.loads(res.content)
-    confirmed = j['latest']['confirmed']
-    deaths = j['latest']['deaths']
-    recovered = j['latest']['recovered']
+    confirmed = int(j['latest']['confirmed'])
+    deaths = int(j['latest']['deaths'])
+    recovered = int(j['latest']['recovered'])
     embed = discord.Embed(
         title=f':beer: Corona update :skull:',
         url='https://www.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6',
         color=0x006534
     )
-    embed.add_field(name="Confirmés", value=confirmed)
-    embed.add_field(name="Morts", value=deaths)
-    embed.add_field(name="Rétablis", value=recovered)
+    embed.add_field(name="Confirmés", value=f'{confirmed:,}')
+    embed.add_field(name="Morts", value=f'{deaths:,}')
+    embed.add_field(name="Rétablis", value=f'{recovered:,}')
 
     return embed
