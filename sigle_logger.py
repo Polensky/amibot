ddd1782f4b88f6188492de3ff664b63c78abb2d1
Charles <sirois.charles.@protonmail.com>
2021-01-22 19:57:46 -0500

    fixing logger

--- sigle_logger.py
+++ sigle_logger.py
@@ -1,8 +1,12 @@
 """
 Custom logger
 """
+import os
 import logging # pylint: disable=no-absolute-import
 from datetime import datetime
+
+
+LOGGING_DIRECTORY_NAME = 'logging'
 
 def start_logger():
     """
@@ -10,12 +14,15 @@ def start_logger():
     """
     logger = logging.getLogger(__name__)
 
+    if not os.path.isdir(LOGGING_DIRECTORY_NAME):
+        os.mkdir(LOGGING_DIRECTORY_NAME)
+
     date = datetime.now()
     date_str = date.strftime('%d-%b-%Y')
 
     # Create handlers
     c_handler = logging.StreamHandler()
-    f_handler = logging.FileHandler(f'logging/amibot_{date_str}.log')
+    f_handler = logging.FileHandler(f'{LOGGING_DIRECTORY_NAME}/amibot_{date_str}.log')
     c_handler.setLevel(logging.INFO)
     f_handler.setLevel(logging.WARNING)
 
